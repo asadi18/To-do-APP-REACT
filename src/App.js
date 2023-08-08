9839fe70a2b12adc4f3c538215b053decc960290
Asad Ishteaque <108184512+asadi18@users.noreply.github.com>
2023-08-08 06:48:33 +0600

    to do app using react

--- src/App.js
+++ src/App.js
@@ -1,23 +1,78 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import "./App.css";
 
 function App() {
+  const [nweItem, setState] = useState("");
+  const [todos, setTodos] = useState(() => {
+    const localvalue = localStorage.getItem("ITEMS");
+    if (localvalue == null) return [];
+    return JSON.parse(localvalue);
+  });
+
+  useEffect(() => {
+    localStorage.setItem("ITEMS", JSON.stringify(todos));
+  }, [todos]);
+
+  function handleSubmit(e) {
+    e.preventDefault();
+
+    setTodos((currentTodos) => {
+      return [
+        ...currentTodos,
+        { id: crypto.randomUUID, title: nweItem, completed: false },
+      ];
+    });
+    setState("");
+  }
+
+  function toggleTodo(id, completed) {
+    setTodos((currentTodos) => {
+      return currentTodos.map((todo) => {
+        if (todo.id === id) {
+          return { ...todo, completed };
+        }
+        return todo;
+      });
+    });
+  }
+
+  function deleteTodo(id) {
+    setTodos((currentTodos) => {
+      return currentTodos.filter((todo) => todo.id !== id);
+    });
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="main-div">
+      <h1>To Do App</h1>
+      <form onSubmit={handleSubmit}>
+        <input
+          value={nweItem}
+          onChange={(e) => setState(e.target.value)}
+          type="text"
+          id="item"
+        />
+        <button>Add</button>
+      </form>
+      <h1>To do List</h1>
+      <ul>
+        {todos.length === 0 && "No To Do to add here"}
+        {todos.map((todo) => {
+          return (
+            <li key={todos.id}>
+              <lable>
+                <input
+                  type="checkbox"
+                  checked={todo.completed}
+                  onChange={(e) => toggleTodo(todo.id, e.target.checked)}
+                />
+                {todo.title}
+              </lable>
+              <button onClick={() => deleteTodo(todo.id)}>Delete</button>
+            </li>
+          );
+        })}
+      </ul>
     </div>
   );
 }
